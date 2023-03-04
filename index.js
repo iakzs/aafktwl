b9c2b34cb5114d6ed0ecf7869f59be3fcc6b950f
not <105176207+iakzs@users.noreply.github.com>
2023-03-03 21:24:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const fs = require('fs');
-
-// Read the contents of the access-token.txt file
-const accessToken = fs.readFileSync('access-token.txt', 'utf8');
-
-const { Octokit } = require("@octokit/rest");
-
-const octokit = new Octokit({
-  auth: process.env.accessToken
-});
-
-// Use the `octokit` instance to make API requests
-// For example, get the current user's information:
-octokit.users.getAuthenticated().then(({ data }) => {
-  console.log(data);
-}).catch(error => {
-  console.error(error);
-});
+//not used
