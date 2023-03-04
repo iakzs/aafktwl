45751c36cfea2a38617d8a5784df876d0bcbc1c0
not <105176207+iakzs@users.noreply.github.com>
2023-03-03 21:16:46 -0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -6,13 +6,13 @@ const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   proxy.web(req, res, {
-    target: 'https://api.github.com',
+    target: 'https://raw.githubusercontent.com',
     changeOrigin: true
   });
 });
 
 server.listen(3000);
 console.log('Proxy server listening on port 3000');
-const response = await fetch('https://api.github.com/repos/iakzs/aafktwl/contents/tkns/vat.js');
+const response = await fetch('https://raw.githubusercontent.com/iakzs/aafktwl/main/tkns/vat.js');
 const data = await response.json();
 console.log(data);
